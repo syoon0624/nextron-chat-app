e1f31eff6a2905362da1144f2d7cb42eaa9d11eb
seungyoonLee <syoon624@naver.com>
2023-01-28 20:17:16 +0900

    feat: JWT에서 로그인 정보 가져오기

---
+++ renderer/utils/formatJWT.ts
@@ -0,0 +1,7 @@
+export const formatJWTtoJSON = (token: string) => {
+  const base64Payload = token.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+  const payload = Buffer.from(base64Payload, 'base64');
+  const result = JSON.parse(payload.toString());
+  console.log(result);
+  return result;
+};
