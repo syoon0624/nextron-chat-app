8aa28e110214841345065783c522ce278a02caed
seungyoonLee <syoon624@naver.com>
2023-01-28 20:15:28 +0900

    feat: recoil 추가

---
+++ renderer/recoil/authAtom.ts
@@ -0,0 +1,17 @@
+import { atom } from 'recoil';
+
+/** 토큰 여부에 따른 로그인 인증 */
+export const authState = atom<string>({
+  key: 'authState',
+  default: '',
+});
+
+export const userState = atom({
+  key: 'userState',
+  default: {
+    isAuthenticated: false,
+    email: '',
+    displayName: '',
+    photoURL: '',
+  },
+});
