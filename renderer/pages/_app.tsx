7e18264f47af62455a67b66f5d92c1f442bcdc89
seungyoonLee <syoon624@naver.com>
2023-01-28 20:16:21 +0900

    feat: 헤더 틀, recoilroot 추가

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import Header from '../components/header/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Header />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { RecoilRoot } from 'recoil';
+import Header from '../components/header/Header';
 
 export default function Document() {
   return (
